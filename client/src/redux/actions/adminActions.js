6e526e90a7fe3039a7a85ceb3e2252a17a9d5953
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-11-08 20:31:39 +0530

    Update adminActions.js
    done

--- client/src/redux/actions/adminActions.js
+++ client/src/redux/actions/adminActions.js
@@ -1,141 +1,248 @@
-import React, { createContext, useContext, useReducer } from 'react';
-import * as api from '../api';
+import {
+  ADMIN_LOGIN,
+  UPDATE_ADMIN,
+  ADD_ADMIN,
+  ADD_DEPARTMENT,
+  ADD_FACULTY,
+  GET_ALL_FACULTY,
+  ADD_SUBJECT,
+  ADD_STUDENT,
+  GET_ALL_STUDENT,
+  GET_FACULTY,
+  GET_SUBJECT,
+  GET_STUDENT,
+  GET_ALL_ADMIN,
+  GET_ALL_DEPARTMENT,
+  SET_ERRORS,
+  UPDATE_PASSWORD,
+  GET_ALL_SUBJECT,
+  DELETE_ADMIN,
+  DELETE_DEPARTMENT,
+  DELETE_FACULTY,
+  DELETE_STUDENT,
+  DELETE_SUBJECT,
+  CREATE_NOTICE,
+  GET_NOTICE,
+} from "../actionTypes";
+import * as api from "../api";
 
-// Create context
-const AdminContext = createContext();
-
-// Action types
-const ADMIN_LOGIN = 'ADMIN_LOGIN';
-const UPDATE_ADMIN = 'UPDATE_ADMIN';
-const ADD_ADMIN = 'ADD_ADMIN';
-const ADD_DEPARTMENT = 'ADD_DEPARTMENT';
-const ADD_FACULTY = 'ADD_FACULTY';
-const GET_ALL_FACULTY = 'GET_ALL_FACULTY';
-const ADD_SUBJECT = 'ADD_SUBJECT';
-const ADD_STUDENT = 'ADD_STUDENT';
-const GET_ALL_STUDENT = 'GET_ALL_STUDENT';
-const GET_FACULTY = 'GET_FACULTY';
-const GET_SUBJECT = 'GET_SUBJECT';
-const GET_STUDENT = 'GET_STUDENT';
-const GET_ALL_ADMIN = 'GET_ALL_ADMIN';
-const GET_ALL_DEPARTMENT = 'GET_ALL_DEPARTMENT';
-const SET_ERRORS = 'SET_ERRORS';
-const UPDATE_PASSWORD = 'UPDATE_PASSWORD';
-const GET_ALL_SUBJECT = 'GET_ALL_SUBJECT';
-const DELETE_ADMIN = 'DELETE_ADMIN';
-const DELETE_DEPARTMENT = 'DELETE_DEPARTMENT';
-const DELETE_FACULTY = 'DELETE_FACULTY';
-const DELETE_STUDENT = 'DELETE_STUDENT';
-const DELETE_SUBJECT = 'DELETE_SUBJECT';
-const CREATE_NOTICE = 'CREATE_NOTICE';
-const GET_NOTICE = 'GET_NOTICE';
-
-// Reducer
-const adminReducer = (state, action) => {
-  switch (action.type) {
-    case ADMIN_LOGIN:
-      return { ...state, adminData: action.payload };
-    case UPDATE_ADMIN:
-    case ADD_ADMIN:
-    case ADD_DEPARTMENT:
-    case ADD_FACULTY:
-    case ADD_SUBJECT:
-    case ADD_STUDENT:
-    case DELETE_ADMIN:
-    case DELETE_DEPARTMENT:
-    case DELETE_FACULTY:
-    case DELETE_STUDENT:
-    case DELETE_SUBJECT:
-    case CREATE_NOTICE:
-      return { ...state, [action.type.toLowerCase()]: action.payload };
-    case GET_ALL_FACULTY:
-    case GET_ALL_STUDENT:
-    case GET_ALL_ADMIN:
-    case GET_ALL_DEPARTMENT:
-    case GET_ALL_SUBJECT:
-    case GET_FACULTY:
-    case GET_SUBJECT:
-    case GET_STUDENT:
-    case GET_NOTICE:
-      return { ...state, [action.type.toLowerCase()]: action.payload };
-    case SET_ERRORS:
-      return { ...state, errors: action.payload };
-    case UPDATE_PASSWORD:
-      return { ...state, passwordUpdated: action.payload };
-    default:
-      return state;
+export const adminSignIn = (formData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.adminSignIn(formData);
+    dispatch({ type: ADMIN_LOGIN, data });
+    if (data.result.passwordUpdated) navigate("/admin/home");
+    else navigate("/admin/update/password");
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
   }
 };
 
-// Provider component
-export const AdminProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(adminReducer, {
-    adminData: null,
-    errors: null,
-    passwordUpdated: false,
-  });
-
-  const adminSignIn = async (formData, navigate) => {
-    try {
-      const { data } = await api.adminSignIn(formData);
-      dispatch({ type: ADMIN_LOGIN, payload: data });
-      if (data.result.passwordUpdated) navigate("/admin/home");
-      else navigate("/admin/update/password");
-    } catch (error) {
-      dispatch({ type: SET_ERRORS, payload: error.response.data });
-    }
-  };
-
-  const adminUpdatePassword = async (formData, navigate) => {
-    try {
-      await api.adminUpdatePassword(formData);
-      dispatch({ type: UPDATE_PASSWORD, payload: true });
-      alert("Password Updated");
-      navigate("/admin/home");
-    } catch (error) {
-      dispatch({ type: SET_ERRORS, payload: error.response.data });
-    }
-  };
-
-  const getAllStudent = async () => {
-    try {
-      const { data } = await api.getAllStudent();
-      dispatch({ type: GET_ALL_STUDENT, payload: data });
-    } catch (error) {
-      console.log("Context Error", error);
-    }
-  };
-
-  // ... Similarly implement other functions ...
-
-  const getNotice = async (formData) => {
-    try {
-      const { data } = await api.getNotice(formData);
-      dispatch({ type: GET_NOTICE, payload: data });
-    } catch (error) {
-      dispatch({ type: SET_ERRORS, payload: error.response.data });
-    }
-  };
-
-  return (
-    <AdminContext.Provider value={{
-      state,
-      adminSignIn,
-      adminUpdatePassword,
-      getAllStudent,
-      // ... Include all other functions here ...
-      getNotice,
-    }}>
-      {children}
-    </AdminContext.Provider>
-  );
+export const adminUpdatePassword = (formData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.adminUpdatePassword(formData);
+    dispatch({ type: UPDATE_PASSWORD, payload: true });
+    alert("Password Updated");
+    navigate("/admin/home");
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
 };
 
-// Custom hook to use the admin context
-export const useAdmin = () => {
-  const context = useContext(AdminContext);
-  if (!context) {
-    throw new Error('useAdmin must be used within an AdminProvider');
+export const getAllStudent = () => async (dispatch) => {
+  try {
+    const { data } = await api.getAllStudent();
+    dispatch({ type: GET_ALL_STUDENT, payload: data });
+  } catch (error) {
+    console.log("Redux Error", error);
   }
-  return context;
+};
+export const getAllFaculty = () => async (dispatch) => {
+  try {
+    const { data } = await api.getAllFaculty();
+    dispatch({ type: GET_ALL_FACULTY, payload: data });
+  } catch (error) {
+    console.log("Redux Error", error);
+  }
+};
+export const getAllAdmin = () => async (dispatch) => {
+  try {
+    const { data } = await api.getAllAdmin();
+    dispatch({ type: GET_ALL_ADMIN, payload: data });
+  } catch (error) {
+    console.log("Redux Error", error);
+  }
+};
+
+export const getAllDepartment = () => async (dispatch) => {
+  try {
+    const { data } = await api.getAllDepartment();
+    dispatch({ type: GET_ALL_DEPARTMENT, payload: data });
+  } catch (error) {
+    console.log("Redux Error", error);
+  }
+};
+
+export const getAllSubject = () => async (dispatch) => {
+  try {
+    const { data } = await api.getAllSubject();
+    dispatch({ type: GET_ALL_SUBJECT, payload: data });
+  } catch (error) {
+    console.log("Redux Error", error);
+  }
+};
+
+export const updateAdmin = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.updateAdmin(formData);
+    dispatch({ type: UPDATE_ADMIN, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const addAdmin = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.addAdmin(formData);
+    alert("Admin Added Successfully");
+    dispatch({ type: ADD_ADMIN, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const createNotice = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.createNotice(formData);
+    alert("Notice Created Successfully");
+    dispatch({ type: CREATE_NOTICE, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const getAdmin = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.getAdmin(formData);
+    dispatch({ type: GET_STUDENT, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const deleteAdmin = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.deleteAdmin(formData);
+    alert("Admin Deleted");
+    dispatch({ type: DELETE_ADMIN, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const deleteFaculty = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.deleteFaculty(formData);
+    alert("Faculty Deleted");
+    dispatch({ type: DELETE_FACULTY, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const deleteStudent = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.deleteStudent(formData);
+    alert("Student Deleted");
+    dispatch({ type: DELETE_STUDENT, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const deleteSubject = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.deleteSubject(formData);
+    alert("Subject Deleted");
+    dispatch({ type: DELETE_SUBJECT, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const deleteDepartment = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.deleteDepartment(formData);
+    alert("Department Deleted");
+    dispatch({ type: DELETE_DEPARTMENT, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+export const addDepartment = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.addDepartment(formData);
+    alert("Department Added Successfully");
+    dispatch({ type: ADD_DEPARTMENT, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const addFaculty = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.addFaculty(formData);
+    alert("Faculty Added Successfully");
+    dispatch({ type: ADD_FACULTY, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const getFaculty = (department) => async (dispatch) => {
+  try {
+    const { data } = await api.getFaculty(department);
+    dispatch({ type: GET_FACULTY, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const addSubject = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.addSubject(formData);
+    alert("Subject Added Successfully");
+    dispatch({ type: ADD_SUBJECT, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const getSubject = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.getSubject(formData);
+    dispatch({ type: GET_SUBJECT, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const addStudent = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.addStudent(formData);
+    alert("Student Added Successfully");
+    dispatch({ type: ADD_STUDENT, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const getStudent = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.getStudent(formData);
+    dispatch({ type: GET_STUDENT, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const getNotice = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.getNotice(formData);
+    dispatch({ type: GET_NOTICE, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
 };
